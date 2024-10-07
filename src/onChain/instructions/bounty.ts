ff72a7e61a8ba3a1876f4f45f328042f5a6aa775
Nkama Williams <williamsnkama@gmail.com>
2024-10-07 10:50:49 +0100

    added: onChain instructions

--- src/onChain/instructions/bounty.ts
+++ src/onChain/instructions/bounty.ts
@@ -16,6 +16,7 @@ import { BountyProgram } from "../types/bountyProgram";
 import * as anchor from "@coral-xyz/anchor";
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
+  getOrCreateAssociatedTokenAccount,
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
 
@@ -134,13 +135,16 @@ export const createBounty = async (
   return signature;
 };
 
-export const applyToBounty = async (
+//Add Submitter
+export const addSubmitter = async (
   wallet: NodeWallet,
   connection: Connection,
   timestamp: string,
+  creatorPk: string, //Creator of bounty application
 ) => {
   const provider = new AnchorProvider(connection, wallet, {});
   const mint = WSOL_ADDRESS;
+  const creator = new PublicKey(creatorPk);
 
   const bounty_program = new Program(
     Bounty_IDL as unknown as BountyProgram,
@@ -161,17 +165,12 @@ export const applyToBounty = async (
 
   const [program_authority] = await findProgramAuthority(bounty_program);
 
-  const createDaoIX = await bounty_program.methods
-    .createFeatureFundingAccount(timestamp)
+  const addSubmitter = await bounty_program.methods
+    .addApprovedSubmitters()
     .accountsPartial({
       creator: wallet.publicKey,
-      fundsMint: mint,
-      featureTokenAccount: feature_token_account,
-      programAuthority: program_authority,
-      tokenProgram: TOKEN_PROGRAM_ID,
-      rent: SYSVAR_RENT_PUBKEY,
-      associatedProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-      systemProgram: SystemProgram.programId,
+      submitter: creator,
+      featureDataAccount: feature_account,
     })
     .instruction();
 
@@ -187,23 +186,284 @@ export const applyToBounty = async (
   };
 
   const tx = new Transaction(txInfo);
-  tx.add(createDaoIX);
+  tx.add(addSubmitter);
   const signature = await provider.sendAndConfirm(tx, [], {
     skipPreflight: true,
   });
-  toast.success("NFT purchased successfully");
+  toast.success("Submitter added successfully");
 
   return signature;
 };
 
 //Fund Bounty
+export const fundAccount = async (
+  wallet: NodeWallet,
+  connection: Connection,
+  timestamp: string,
+  amount: Number,
+) => {
+  const provider = new AnchorProvider(connection, wallet, {});
+  const mint = WSOL_ADDRESS;
+  const creatorTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    wallet.payer,
+    mint,
+    wallet.publicKey,
+  );
 
-//Accept Applications
+  const bounty_program = new Program(
+    Bounty_IDL as unknown as BountyProgram,
+    provider,
+  );
+  // const timestamp = Date.now().toString();
+  const [feature_account] = await findFeatureAccount(
+    timestamp,
+    wallet.publicKey,
+    bounty_program,
+  );
+  const [feature_token_account] = await findFeatureTokenAccount(
+    timestamp,
+    wallet.publicKey,
+    mint,
+    bounty_program,
+  );
 
-//Reject Applications
+  const [program_authority] = await findProgramAuthority(bounty_program);
+
+  const fund = await bounty_program.methods
+    .fundFeature(new BN(amount))
+    .accountsPartial({
+      creator: wallet.publicKey,
+      creatorTokenAccount: creatorTokenAccount.address,
+      featureTokenAccount: feature_token_account,
+      featureDataAccount: feature_account,
+      fundsMint: mint,
+      programAuthority: program_authority,
+      systemProgram: SystemProgram.programId,
+      tokenProgram: TOKEN_PROGRAM_ID,
+    })
+    .instruction();
+
+  const { blockhash, lastValidBlockHeight } =
+    await provider.connection.getLatestBlockhash();
+  const txInfo = {
+    /** The transaction fee payer */
+    feePayer: wallet.publicKey,
+    /** A recent blockhash */
+    blockhash: blockhash,
+    /** the last block chain can advance to before tx is exportd expired */
+    lastValidBlockHeight: lastValidBlockHeight,
+  };
+
+  const tx = new Transaction(txInfo);
+  tx.add(fund);
+  const signature = await provider.sendAndConfirm(tx, [], {
+    skipPreflight: true,
+  });
+  toast.success("Funds paid successfully");
+
+  return signature;
+};
+
+//Submit Feature
+export const submitFeature = async (
+  wallet: NodeWallet,
+  connection: Connection,
+  timestamp: string,
+  creatorPk: string, //Bounty Creator
+) => {
+  const provider = new AnchorProvider(connection, wallet, {});
+  const mint = WSOL_ADDRESS;
+  const creator = new PublicKey(creatorPk);
+  const payoutAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    wallet.payer,
+    mint,
+    wallet.publicKey,
+  );
+
+  const bounty_program = new Program(
+    Bounty_IDL as unknown as BountyProgram,
+    provider,
+  );
+  // const timestamp = Date.now().toString();
+  const [feature_account] = await findFeatureAccount(
+    timestamp,
+    creator,
+    bounty_program,
+  );
+  // const [feature_token_account] = await findFeatureTokenAccount(
+  //   timestamp,
+  //   creator,
+  //   mint,
+  //   bounty_program,
+  // );
+
+  // const [program_authority] = await findProgramAuthority(bounty_program);
+
+  const submit = await bounty_program.methods
+    .submitRequest()
+    .accountsPartial({
+      creator,
+      submitter: wallet.publicKey,
+      featureDataAccount: feature_account,
+      payoutAccount: payoutAccount.address,
+    })
+    .instruction();
+
+  const { blockhash, lastValidBlockHeight } =
+    await provider.connection.getLatestBlockhash();
+  const txInfo = {
+    /** The transaction fee payer */
+    feePayer: wallet.publicKey,
+    /** A recent blockhash */
+    blockhash: blockhash,
+    /** the last block chain can advance to before tx is exportd expired */
+    lastValidBlockHeight: lastValidBlockHeight,
+  };
+
+  const tx = new Transaction(txInfo);
+  tx.add(submit);
+  const signature = await provider.sendAndConfirm(tx, [], {
+    skipPreflight: true,
+  });
+  toast.success("Submitted successfully");
+
+  return signature;
+};
 
 //Accept Feature
+export const acceptFeature = async (
+  wallet: NodeWallet,
+  connection: Connection,
+  timestamp: string,
+  creatorPK: string, //Submitter of feature
+) => {
+  const provider = new AnchorProvider(connection, wallet, {});
+  const mint = WSOL_ADDRESS;
+  const featureCreator = new PublicKey(creatorPK);
+  const payoutAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    wallet.payer,
+    mint,
+    featureCreator,
+  );
+
+  const bounty_program = new Program(
+    Bounty_IDL as unknown as BountyProgram,
+    provider,
+  );
+  // const timestamp = Date.now().toString();
+  const [feature_account] = await findFeatureAccount(
+    timestamp,
+    wallet.publicKey,
+    bounty_program,
+  );
+  const [feature_token_account] = await findFeatureTokenAccount(
+    timestamp,
+    wallet.publicKey,
+    mint,
+    bounty_program,
+  );
+
+  const [program_authority] = await findProgramAuthority(bounty_program);
+
+  const approve = await bounty_program.methods
+    .approveRequest()
+    .accountsPartial({
+      creator: wallet.publicKey,
+      submitter: featureCreator,
+      featureDataAccount: feature_account,
+      featureTokenAccount: feature_token_account,
+      payoutAccount: payoutAccount.address,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      programAuthority: program_authority,
+    })
+    .instruction();
+
+  const { blockhash, lastValidBlockHeight } =
+    await provider.connection.getLatestBlockhash();
+  const txInfo = {
+    /** The transaction fee payer */
+    feePayer: wallet.publicKey,
+    /** A recent blockhash */
+    blockhash: blockhash,
+    /** the last block chain can advance to before tx is exportd expired */
+    lastValidBlockHeight: lastValidBlockHeight,
+  };
+
+  const tx = new Transaction(txInfo);
+  tx.add(approve);
+  const signature = await provider.sendAndConfirm(tx, [], {
+    skipPreflight: true,
+  });
+  toast.success("Approved and payout successful");
+
+  return signature;
+};
 
 //Reject Feature
+export const rejectFeature = async (
+  wallet: NodeWallet,
+  connection: Connection,
+  timestamp: string,
+  creatorPK: string, //Submitter of feature
+) => {
+  const provider = new AnchorProvider(connection, wallet, {});
+  const mint = WSOL_ADDRESS;
+  const featureCreator = new PublicKey(creatorPK);
+  const payoutAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    wallet.payer,
+    mint,
+    featureCreator,
+  );
 
-//Withdrawal
+  const bounty_program = new Program(
+    Bounty_IDL as unknown as BountyProgram,
+    provider,
+  );
+  // const timestamp = Date.now().toString();
+  const [feature_account] = await findFeatureAccount(
+    timestamp,
+    wallet.publicKey,
+    bounty_program,
+  );
+  const [feature_token_account] = await findFeatureTokenAccount(
+    timestamp,
+    wallet.publicKey,
+    mint,
+    bounty_program,
+  );
+
+  const [program_authority] = await findProgramAuthority(bounty_program);
+
+  const reject = await bounty_program.methods
+    .denyRequest()
+    .accountsPartial({
+      creator: wallet.publicKey,
+      submitter: featureCreator,
+      featureDataAccount: feature_account,
+    })
+    .instruction();
+
+  const { blockhash, lastValidBlockHeight } =
+    await provider.connection.getLatestBlockhash();
+  const txInfo = {
+    /** The transaction fee payer */
+    feePayer: wallet.publicKey,
+    /** A recent blockhash */
+    blockhash: blockhash,
+    /** the last block chain can advance to before tx is exportd expired */
+    lastValidBlockHeight: lastValidBlockHeight,
+  };
+
+  const tx = new Transaction(txInfo);
+  tx.add(reject);
+  const signature = await provider.sendAndConfirm(tx, [], {
+    skipPreflight: true,
+  });
+  toast.success("Rejection successful");
+
+  return signature;
+};
