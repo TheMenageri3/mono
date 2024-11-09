3864089a3fdddb0e1ad5845e5ce78f9c10d008e3
jacksturt <jack@sturt.io>
2024-11-09 10:51:21 -0800

    require signin

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,14 @@
 import { CreateProfile } from "~/content/signup/content";
 import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "~/server/auth";
 
 export default async function SignupPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-darkness-300">
       <Image
