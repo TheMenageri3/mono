4426e9e90eecfaf52e63c597d5ce570810d28d85
qhristen <odeyxtian@gmail.com>
2025-04-16 21:23:25 +0100

    update route

--- src/server/api/routers/jobApplication/update.tsx
+++ src/server/api/routers/jobApplication/update.tsx
@@ -1,4 +1,3 @@
-import { TRPCError } from "@trpc/server";
 import { protectedProcedure } from "../../trpc";
 import { updateJobApplicationSchema } from "./schema";
 
@@ -7,13 +6,17 @@ export const updateJobApplication = protectedProcedure
   .mutation(async ({ ctx, input }) => {
     const userId = ctx.session?.user?.id;
 
+    const existingJobApplication = await ctx.db.jobApplication.findUniqueOrThrow({
+      where: { id: input.jobApplicationId },
+    });
+
     try {
       const updatedJobApplication = await ctx.db.jobApplication.update({
-        where: { id: input.jobApplicationId },
+        where: { id: existingJobApplication.id },
         data: {
           ...input,
           updatedById: userId,
-
+          
         },
       });
         return updatedJobApplication;
