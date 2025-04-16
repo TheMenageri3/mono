7b3e29605a6a443957ac84886f2261d475cdf9a6
NAJIB-B <najibajibasu@gmail.com>
2025-04-16 10:18:18 +0100

    Implementd create and read endpoint for section

---
+++ src/server/api/routers/section/create.tsx
@@ -0,0 +1,41 @@
+import { protectedProcedure } from "@/server/api/trpc";
+import { any, z } from "zod";
+import { TRPCError } from "@trpc/server";
+
+export const createSection = protectedProcedure
+	.input(
+		z.object({
+			header: z.string(),
+			metadata: z.record(z.any())
+		})
+	)
+	.mutation(async ({ctx, input}) => {
+		const userId = ctx.session?.user?.id;
+
+		if (!userId) {
+			throw new TRPCError({
+				code: "UNAUTHORIZED",
+				message: "User not authenticated",
+			})
+		}
+
+		try {
+			const wallet = await ctx.db.section.create({
+				data: {
+				  header: input.header,
+				  metadata: input.metadata,
+				},
+			  });
+			  return wallet;
+			
+		} catch (error) {
+			console.error("Error creating section:", error);
+			throw new TRPCError({
+			  code: "INTERNAL_SERVER_ERROR",
+			  message: "Failed to create section",
+			  cause: error,
+			});
+		}
+
+
+	})
