1b7c0707cd6770342bfd276a41ff5cf3e14c0042
qhristen <odeyxtian@gmail.com>
2025-04-16 15:15:58 +0100

    remove unused import

--- src/server/api/routers/jobApplication/read.tsx
+++ src/server/api/routers/jobApplication/read.tsx
@@ -1,4 +1,3 @@
-import { db } from "@/server/db";
 import { publicProcedure } from "@/server/api/trpc";
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
@@ -9,38 +8,38 @@ export const getJobApplicationById = publicProcedure
       id: z.string(),
     })
   )
-  .query(async ({ input }: { input: { id: string } }) => {
-    const jobApplication = await db.jobApplication.findFirst({
+  .query(async ({ ctx, input }) => {
+    const jobApplication = await ctx.db.jobApplication.findFirst({
       where: {
-        id: input.id
+        id: input.id,
       },
     });
     return jobApplication;
   });
 
+export const readAllJobApplications = publicProcedure.query(async ({ ctx }) => {
+  try {
+    const jobApplications = await ctx.db.jobApplication.findMany({
+      where: {
+        deletedAt: null,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+    return jobApplications;
+  } catch (error) {
+    console.error("Error getting job applications:", error);
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Failed to get job applications",
+      cause: error,
+    });
+  }
+});
 
-  export const readAllJobApplications = publicProcedure.query(async ({ ctx }) => {
-    try {
-      const jobApplications = await ctx.db.jobApplication.findMany({
-        where: {
-          deletedAt: null,
-        },
-        orderBy: {
-          updatedAt: "desc",
-        },
-      });
-      return jobApplications;
-    } catch (error) {
-      console.error("Error getting job applications:", error);
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Failed to get job applications",
-        cause: error,
-      });
-    }
-  });
-
-  export const readDeletedJobApplications = publicProcedure.query(async ({ ctx }) => {
+export const readDeletedJobApplications = publicProcedure.query(
+  async ({ ctx }) => {
     try {
       const jobApplications = await ctx.db.jobApplication.findMany({
         where: {
@@ -61,4 +60,5 @@ export const getJobApplicationById = publicProcedure
         cause: error,
       });
     }
-  });
+  }
+);
