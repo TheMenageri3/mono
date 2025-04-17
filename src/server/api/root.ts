1c72df685f67a845620a513325e078443835f1a3
Allen-Saji <allensaji04@gmail.com>
2025-04-17 16:05:50 +0530

    add company routes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,6 +7,7 @@ import { jobAppicationRouter } from "./routers/jobApplication";
 import { jobPostingRouter } from "./routers/jobPosting";
 import { placementFeedbackRouter } from "./routers/placementFeedback";
 import { classroomRouter } from "./routers/classroom";
+import { companyRouter } from "./routers/company";
 
 /**
  * This is the primary router for your server.
@@ -22,6 +23,7 @@ export const appRouter = createTRPCRouter({
   jobPosting: jobPostingRouter,
   placementFeedback: placementFeedbackRouter,
   classroom: classroomRouter,
+  company: companyRouter,
 });
 
 // export type definition of API
