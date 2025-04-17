f71b9a5bf98a132287a939fdf18badfcb5b87cb6
Adetola Adedeji <adetolaadedeji0@gmail.com>
2025-04-17 12:23:47 +0100

    Add , in root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -12,7 +12,7 @@ export const appRouter = createTRPCRouter({
   user: userRouter,
   tag: tagRouter,
   wallet: walletRouter,
-  industry: industryRouter
+  industry: industryRouter,
 });
 
 // export type definition of API
