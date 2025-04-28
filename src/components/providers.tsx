b8911be4207323725b5cce9f47ae282bd20618ff
jacksturt <jack@sturt.io>
2025-04-28 08:25:00 -0600

    rm unused trpc/rect query providers

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,20 +5,18 @@ import { SessionProvider } from "next-auth/react";
 import { ReactQueryProvider } from "../app/react-query-provider";
 import { ClusterProvider } from "./cluster/cluster-data-access";
 import { SolanaProvider } from "./solana/solana-provider";
-import { TRPCProvider } from "@/app/trpc-provider";
+import { TRPCReactProvider } from "@/trpc/react";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider>
       <SessionProvider>
-        <TRPCProvider>
-          <ReactQueryProvider>
-            <ClusterProvider>
-              <SolanaProvider>{children}</SolanaProvider>
-            </ClusterProvider>
-          </ReactQueryProvider>
-        </TRPCProvider>
+        <TRPCReactProvider>
+          <ClusterProvider>
+            <SolanaProvider>{children}</SolanaProvider>
+          </ClusterProvider>
+        </TRPCReactProvider>
       </SessionProvider>
     </ThemeProvider>
   );
