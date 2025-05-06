59d93cb150a8fa5ed8109337616dc368f519008b
AtoMicKraK1n <prakharsharma2506@gmail.com>
2025-05-06 14:48:12 +0400

    trying again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,9 @@ const links: { label: string; path: string }[] = [
 
 // Testing dropdown items
 const testingLinks: { label: string; path: string }[] = [
+  { label: "AssignmentQuestion", path: "/testing/assignmentQuestion" },
   { label: "Components", path: "/testing/components" },
   { label: "Tags", path: "/testing/tags" },
-  { label: "AssignmentQuestion", path: "/testing/assignmentQuestion" },
-  { label: "Assignment Submission Answers", path: "/testing/assignmentSubmissionAnswer" },
-  { label: "Assignment Submissions", path: "/testing/assignmentSubmission" },
-  { label: "Enrollment", path: "/testing/enrollment" },
-  { label: "Users", path: "/testing/assignment" },
 ];
 
 export default function RootLayout({
