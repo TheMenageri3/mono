e7009a0ff059de47aef6067fa3760f3be987d8df
Ayush <ayushagarwal.dev@gmail.com>
2025-05-09 17:18:21 +0530

    typescript fix

--- src/app/testing/interview/components/ListInterviews.tsx
+++ src/app/testing/interview/components/ListInterviews.tsx
@@ -207,7 +207,7 @@ export default function ListInterviews() {
           >
           </form> */}
           <Select
-            onValueChange={(val) => {
+            onValueChange={(val: InterviewType) => {
               setSearchTitle(val);
             }}
             defaultValue={searchTitle}
