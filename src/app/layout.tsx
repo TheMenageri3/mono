e02ea032ea9ab9ed36180ca8f429f4fd3b58cd4f
AtoMicKraK1n <prakharsharma2506@gmail.com>
2025-05-09 11:08:38 +0400

    resolving conflicts v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,13 @@ const links: { label: string; path: string }[] = [
 
 // Testing dropdown items
 const testingLinks: { label: string; path: string }[] = [
+  { label: "Admin Comment", path: "/testing/adminComment" },
   { label: "Components", path: "/testing/components" },
   { label: "Tags", path: "/testing/tags" },
   { label: "Assignment Submission Answers", path: "/testing/assignmentSubmissionAnswer" },
   { label: "Assignment Submissions", path: "/testing/assignmentSubmission" },
   { label: "Enrollment", path: "/testing/enrollment " },
   { label: "Users", path: "/testing/assignment" },
-  { label: "Admin Comment", path: "/testing/adminComment" },
   { label: "AssignmentQuestion", path: "/testing/assignmentQuestion" },
 ];
 
