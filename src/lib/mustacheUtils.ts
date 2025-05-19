b417f895d571d7b9eae25c55c514b6a172490bdd
zsh28 <zeeshan8210@outlook.com>
2025-05-19 06:19:19 +0100

    fix mustache

--- src/lib/mustacheUtils.ts
+++ src/lib/mustacheUtils.ts
@@ -1,8 +1,8 @@
-const Mustache = require("mustache");
+import Mustache from "mustache";
 
 export function extractMustacheVariables(template: string): string[] {
   const tokens = Mustache.parse(template);
   return tokens
-    .filter(([type]: [string]) => type === "name")
-    .map(([, name]: [string, string]) => name);
+    .filter(([type]) => type === "name")
+    .map(([, name]) => name);
 }
